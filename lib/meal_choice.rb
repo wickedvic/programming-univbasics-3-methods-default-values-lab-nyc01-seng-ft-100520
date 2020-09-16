a90bb27e57c0d0b13e7a61b19aefd8bf9b6abba3
Vivek Sanghvi <viksipart@gmail.com>
2020-09-16 00:36:17 +0000

    Done.

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,2 +1,9 @@
 # Your code here
 # For output purposes, use "puts" instead of "print" or "p"
+
+def meal_choice(choice1, choice2, protein = "tofu")
+  puts "What a nutritious meal!"
+  meal = "A plate of #{protein} with #{choice1} and #{choice2}."
+  puts meal 
+  return meal
+end
